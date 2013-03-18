296dda22f603a08e82940e230e99406955770da1
xadhix <xadhix@gmail.com>
2013-03-18 16:28:01 +0530

    Deal, Render

--- src-test/cards-test.js
+++ src-test/cards-test.js
@@ -6,10 +6,87 @@
  * To change this template use File | Settings | File Templates.
  */
 
+describe("Check Deck", function () {
 
-describe("Basic Checks", function () {
-    it("Check if 'Cards' is defined", function () {
-        expect(Cards).toBeDefined();
+    var deck;
+    var jokerDeck;
+
+    beforeEach(function () {
+        deck = new Deck();
+        jokerDeck = new Deck(true);
     });
 
+    it("Check if current number of cards are present", function () {
+        expect(deck.cards.length).toBe(52);
+        expect(jokerDeck.cards.length).toBe(54);
+    });
+
+    //TODO : Check if all cards are present
+
+    it("Check if shuffle is working", function () {
+        var beforeString = deck.cards.join("");
+        deck.shuffle();
+        var deckAfterShuffling = deck.cards.slice(0);
+
+        while (beforeString != "") {
+            var card = beforeString[0] + beforeString[1];
+            var indexOfCard = deckAfterShuffling.indexOf(card);
+            expect(indexOfCard).toBeGreaterThan(-1);
+            if (beforeString.length > 1) {
+                beforeString = beforeString.substring(2);
+                deckAfterShuffling.splice(indexOfCard, 1);
+            }
+            else {
+                return false;
+            }
+        }
+        expect(deckAfterShuffling.join("")).toBe("");
+    });
+
+    //TODO : Check Efficiency of Shuffling Algorithm
+
+});
+
+describe("Check Player", function () {
+    it("Basic checks", function () {
+        var p1 = new Player(0);
+        expect(p1.id).toBe("Player");
+        expect(p1.cardsState.length).toBe(Game.numberOfPlayers);
+        var p2 = new Player(1, "xadhix");
+        expect(p2.id).toBe("xadhix");
+        expect(p2.cardsState.length).toBe(Game.numberOfPlayers);
+    });
+});
+
+describe("Check Game", function () {
+    it("Check Number Of Players", function () {
+        expect(Game.numberOfPlayers).toBe(4);
+        var n = 5;
+        Game.setNumberOfPlayers(n);
+        expect(Game.numberOfPlayers).toBe(n);
+        Game.setNumberOfPlayers(4);
+    });
+
+    it("Check whether cards are dealt properly", function () {
+        function checkDealtCards(dealt, num) {
+            if (dealt.length != num) {
+                return false;
+            }
+            var totalNumberOfCards = 0;
+            for (var i = 0; i < num; i++) {
+                if (!(typeof(dealt[i]) == "object" && typeof(dealt[i].length) == "number")) {
+                    return false;
+                }
+                totalNumberOfCards += dealt[i].length;
+            }
+            return true;
+        }
+
+        var n = 4;
+        var dealt1 = Game.deal({"shuffle":true});
+        expect(checkDealtCards(dealt1, n)).toBe(true);
+        var n = 6;
+        var dealt2 = Game.deal({"n":6, "startingFrom":0});
+        expect(checkDealtCards(dealt2, n)).toBe(true);
+    });
 });
--- src/cards.js
+++ src/cards.js
@@ -6,23 +6,108 @@
  * To change this template use File | Settings | File Templates.
  */
 
-Cards = {};
-
-Cards.deck = new Deck();
+var Cards = {
+}
+var Game = {}
 
 function Deck(withJoker) {
-    this.cards = ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK", "HA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "CA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJ", "SQ", "SK", "SA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK", "DA"];
-    if (withJoker) {
+    this.cards = ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HX", "HJ", "HQ", "HK", "HA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CX", "CJ", "CQ", "CK", "CA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "SX", "SJ", "SQ", "SK", "SA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DX", "DJ", "DQ", "DK", "DA"];
+    if (withJoker === true) {
         this.cards.push("J1");
         this.cards.push("J2");
     }
+
     this.shuffle = function () {
         for (var i = 0; i < this.cards.length; i++) {
-            var rand = Math.round(Math.random() * (this.cards.length - i) + i);
-            console.log(rand);
+            var rand = Math.round(Math.random() * (this.cards.length - i - 1) + i);
             var a = this.cards[i];
             this.cards[i] = this.cards[rand];
             this.cards[rand] = a;
         }
+        return this;
     }
+    return this;
+}
+
+function Player(idx, id) {
+    this.id = (id) ? id : "Player";
+    this.idx = idx;
+    this.cardsState = new Array(Game.numberOfPlayers);
+    this.setState = function setState(owncards) {
+        for (var i = 0; i < this.cardsState.length; i++) {
+            this.cardsState[i] = {};
+        }
+        var deckOfCards = Game.deck.cards;
+        for (var i = 0; i < deckOfCards.length; i++) {
+            var self, others;
+            if (owncards.indexOf(deckOfCards[i]) >= 0) {
+                self = 1;
+                others = -1;
+            }
+            else {
+                self = -1;
+                others = 0;
+            }
+            for (var j = 0; j < this.cardsState.length; j++) {
+                this.cardsState[j][deckOfCards[i]] = (this.idx == j) ? self : others;
+            }
+        }
+        return this;
+    }
+
+    this.cards = function cards() {
+        var hasThese = [];
+        for (var card in this.cardsState[this.idx]) {
+            if (this.cardsState[this.idx][card] == 1) {
+                hasThese.push(card);
+            }
+        }
+        return hasThese;
+    }
+    return this;
+}
+
+Game.deck = new Deck();
+Game.numberOfPlayers = 4;
+Game.setNumberOfPlayers = function setNumberOfPlayers(n) {
+    Game.numberOfPlayers = n;
+    return this;
+}
+
+Game.deal = function deal(config) {
+    var n = Game.numberOfPlayers;
+    var startingFrom = 0;
+    //TODO : Accomodate config.
+    if (!config) {
+        config = {};
+    }
+    if (config["shuffle"] === true) {
+        Game.deck.shuffle();
+    }
+    if (config["n"] && typeof(config["n"]) == "number" && config["n"] > 0) {
+        n = config["n"];
+    }
+    if (config["startingFrom"] !== "undefined" && typeof(config["startingFrom"]) == "number" && config["startingFrom"] >= 0 && config["startingFrom"] < n) {
+        startingFrom = config["startingFrom"];
+    }
+    var cards = new Array();
+    for (var i = 0; i < n; i++) {
+        cards.push(new Array());
+    }
+
+    for (var i = 0; i < Game.deck.cards.length; i++) {
+        cards[startingFrom].push(Game.deck.cards[i]);
+        startingFrom = (startingFrom + 1) % n;
+    }
+    return cards;
+}
+
+Game.init = function init() {
+    var dealtCards = Game.deal({"shuffle":true});
+    var players = [];
+    for (var i = 0; i < this.numberOfPlayers; i++) {
+        players.push((new Player(i, "Player" + (i + 1))).setState(dealtCards[i]));
+    }
+    Game.players = players;
+    return this;
 }
